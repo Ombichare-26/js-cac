4b3d9295c620a2d2cb223e0f800e4e65be980f1c
Ombichare-26 <ombichare2006@gmail.com>
2025-12-16 07:38:33 +0000

    comm var & datatypes

---
+++ 01_Basics/01_Variables.js
@@ -0,0 +1,7 @@
+const accId = 1454;
+let accemail = "om@gmail.com";
+var accpass = 1234;
+
+// accId =2;
+
+console.log(accId);
---
+++ 01_Basics/02_DataTypes.js
@@ -0,0 +1,23 @@
+let score = "33a";
+console.log(typeof score);
+let newscore = Number(score);
+console.log(typeof newscore);
+console.log(newscore);
+
+let num = 0
+let boolnum = Boolean(num);
+
+console.log(typeof boolnum);
+console.log(boolnum);
+
+console.log("1"+0+"1");
+
+let out;
+console.log(typeof out);;
+
+const myfunction= ()=>{
+    console.log("Hello World");
+    
+};
+
+myfunction();
