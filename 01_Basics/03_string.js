d0fad233ad20e6f3017df962e491a6a0dd3feedc
Ombichare-26 <ombichare2006@gmail.com>
2025-12-17 07:47:41 +0000

    add strings

---
+++ 01_Basics/03_string.js
@@ -0,0 +1,19 @@
+// const name = "Om"
+// const age = 19
+
+// console.log(`Hello, My name is ${name} and i am ${age} old`);
+
+// const newgame = String('ombichare');
+// console.log(newgame[0]);
+// console.log(newgame.__proto__);
+// console.log(newgame.length);
+// console.log(newgame.toLocaleUpperCase());
+// console.log(newgame.indexOf('m'));
+// console.log(newgame.substring(1,3));
+
+const newgame = String('     om    bichare    .')
+console.log(newgame);
+console.log(
+
+    newgame.trim()
+);
