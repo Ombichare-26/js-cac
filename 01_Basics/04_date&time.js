7f0fe378900dce92817278869be8d4c8930dc126
Ombichare-26 <ombichare2006@gmail.com>
2025-12-17 11:06:51 +0000

    add date

---
+++ 01_Basics/04_date&time.js
@@ -0,0 +1,18 @@
+//Dates
+
+// let myDate = new Date();
+// console.log(myDate.toString());
+// console.log(myDate.toDateString());
+// console.log(myDate.toISOString());
+// console.log(myDate.toJSON());
+// console.log(myDate.toLocaleString());
+
+// const myTimeStamp = Date.now();
+// console.log(myTimeStamp);
+
+let newdate = new Date();
+console.log(newdate.getDate());
+console.log(newdate.getDay());
+console.log(newdate.getMonth());
+
+console.log(newdate.getTime());
