57a3f781822449770fd3e1faf6259663326b5842
Ombichare-26 <ombichare2006@gmail.com>
2025-12-18 16:39:27 +0000

    for & while & do_while

---
+++ 04_iterations/two.js
@@ -0,0 +1,18 @@
+//while
+
+// let i = 0
+
+// while(i<=10){
+//     console.log(`Value of i is ${i}`);
+//     i++;
+// }
+
+// do while
+
+// let score = 11;
+
+// do{
+//     console.log(`Score is ${score}`);
+//     score++;
+    
+// }while(score<=10);
