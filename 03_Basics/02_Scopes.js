d776caa7a8fa9da577248a155e5df85b98f735df
Ombichare-26 <ombichare2006@gmail.com>
2025-12-18 14:47:57 +0000

    add scopes

---
+++ 03_Basics/02_Scopes.js
@@ -0,0 +1,18 @@
+// function one (){
+//     const username = "omBichare"
+
+//     function two(){
+//         const website = "youtube";
+//         console.log(username);
+        
+//     }
+//     //console.log(website);
+//     two();
+// }
+// one();
+
+const addnum = (num)=>{
+    return num + 1;
+}
+
+console.log(addnum(9));
