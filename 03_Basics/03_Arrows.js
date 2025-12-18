aa89d1698d262beb55dc3ef2eda3bf73e8af5312
Ombichare-26 <ombichare2006@gmail.com>
2025-12-18 15:13:36 +0000

    add arrow_function

---
+++ 03_Basics/03_Arrows.js
@@ -0,0 +1,19 @@
+// const user = {
+//     username : "omBichare",
+//     price: 399,
+//     welcomemessage: function(){
+//         console.log(`${this.username}, welcome to the website.`);
+        
+//     }
+// }
+// user.welcomemessage();
+// user.username= "om";
+// user.welcomemessage();
+
+// function chai(){
+//     username: "OmBichare"
+//     console.log(`${this.username}`);
+ 
+    
+// }
+// chai();
